47fd6e23b9f714be389824130d55d59e5c150199
Fabio Petrone <fa610movic90@gmail.com>
2021-02-18 14:31:22 +0100

    parte principale main

--- action.js
+++ action.js
@@ -9,7 +9,24 @@ new Vue({
         { navElement: 'About' },
         { navElement: 'Videos' },
         { navElement: 'Blog' }
-      ]
+      ],
+      videosData:[
+        {
+          photo: 'img/video2-2x.jpg',
+          title: 'Thighs & glute workout',
+          description:'Increase your mobility',
+        },
+        {
+          photo: 'img/video7-2x.jpg',
+          title: 'Lift, firm & perk up',
+          description:'feel young again',
+        },
+        {
+          photo: 'img/video9-2x.jpg',
+          title: 'Slim & trim your waist',
+          description:'Shed those extra pounds',
+        }
+      ],
   },
   methods:{
 
